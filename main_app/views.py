aa0fe957e1d7fa667e7e69711d38827806696eed
nasir <nasir@Nasirs-MBP.fios-router.home>
2020-04-14 20:54:50 -0400

    pull api data

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from newsapi import NewsApiClient
 
 
 def home(request):
-    return render(request, 'home.html')
+    newsapi = NewsApiClient(api_key='313c12fcfda3465f89f36b377c088e3b')
+    top = newsapi.get_top_headlines(q='coronavirus',
+                                    language='en')
+
+    l = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, 'home.html', context={"mylist": mylist})
 
 
 def about(request):
