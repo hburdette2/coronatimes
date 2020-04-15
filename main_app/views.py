ad866f74f10682393d9ec6fe90f5280a42fa6dba
Frank Rauen <home@Franks-MacBook-Air.local>
2020-04-14 19:23:22 -0600

    updates to match Nasir

--- main_app/views.py
+++ main_app/views.py
@@ -11,7 +11,24 @@ from newsapi import NewsApiClient
 
 
 def home(request):
-    return render(request, 'home.html')
+    newsapi = NewsApiClient(api_key='313c12fcfda3465f89f36b377c088e3b')
+    top = newsapi.get_top_headlines(q='coronavirus',
+                                    language='en')
+
+    l = top['articles']
+    desc = []
+    news = []
+    img = []
+
+    for i in range(len(l)):
+        f = l[i]
+        news.append(f['title'])
+        desc.append(f['description'])
+        img.append(f['urlToImage'])
+    mylist = zip(news, desc, img)
+
+    return render(request, 'home.html', context={"mylist": mylist})
+
 
 
 def about(request):
